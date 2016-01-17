1a1dcac8b3e817bf15a6f50094ee03da3caad7a4
camelaissani <camel.aissani@nuageprive.fr>
2016-01-17 14:05:20 +0100

    Clean some stuff

--- js/index.js
+++ js/index.js
@@ -1,44 +1,39 @@
+var app1 = null;
+var app2 = null;
+var homescreen;
+var cslpre = '!!-';
+
 SplitScreen = typeof SplitScreen === 'undefined' ? {
     originalResize: null,
     date: new Date()
 } : SplitScreen;
 console.log('**********************************\nSplit screen addon loaded\n' + SplitScreen.date + '\n**********************************');
-// if (document.readyState === 'complete') {
-//     console.log('document state is complete');
-//         initialize();
-// } else {
-//     console.log('document is not ready');
-//     window.addEventListener('load', () => {
-//         console.log('now document state is complete');
-//         initialize();
-//     });
-// }
+
+if (document.readyState === 'complete') {
+    console.log('document state is complete');
+    homescreen = document.querySelector('#homescreen');
+    console.log("on a le homescreen" + homescreen);
+} else {
+    console.log('document is not ready');
+    window.addEventListener(listeners.load.event, listeners.load.callback);
+}
 
 const listeners = {
+    load: {
+        event: 'load',
+        callback: function(event) {
+            console.log('now document state is complete');
+            homescreen = document.querySelector('#homescreen');
+            console.log("on a le homescreen" + homescreen);
+        }
+    },
     appresize: {
         event: 'appresize',
         callback: function(event) {
-            console.log('appresize');
             var appWindow = event.detail;
-
-            console.log("on va gen_split",appWindow.element.style);
-//            appWindow.element.style.height = Math.floor(appWindow.element.style.height.replace('px', '') / 2) + 'px';
-//            gen_split("on a gen_split",appWindow);
-          if(!homescreen.classList.contains('active')){
-            console.log(cslpre+' on est pas dans le homescreen');
-            var active = document.querySelector('#windows .active');
-            if(app1==null){
-              console.log(cslpre+' on a pas encore une app');
-                show2(active,null);
-            }else{
-              console.log(cslpre+' on a déjà une app');
-              show2(app1,active);
-            }
-          }else{
-            console.log(cslpre+' on retourne sur le home');
-            show1(null);
-          }
-          console.log(cslpre+' on a retaillé');
+            console.log('appresize');
+            console.log("on va gen_split", appWindow.element.style);
+            gen_split(appWindow);
             console.log(appWindow.element.style);
         }
     },
@@ -56,69 +51,70 @@ const listeners = {
             }
         }
     },
-    appwillclose : {
+    appwillclose: {
         event: 'appwillclose',
-    	callback: function(event) {
-          console.log(cslpre+' on a appwillclose');
-          if(!homescreen.classList.contains('active'))
-              show2(app1,app2);
-          else
-              show1(null);
+        callback: function(event) {
+            console.log(cslpre + ' on a appwillclose');
+            if (!homescreen.classList.contains('active'))
+                show2(app1, app2);
+            else
+                show1(null);
         }
     }
 };
 
-var app1 = null ;
-var app2 = null ;
-var homescreen ;
-var cslpre = '!!-';
-
-function show2(first,second){
-  app1 = first ;
-  if(first==null)
-   first = homescreen;
-  app2 = second ;
-  if(second==null)
-   second = homescreen;
-  console.log(cslpre+' on va modif les css vers 2');
-  first.style.height = '50%' ;
-  second.style.transform = 'translate(0,0)';
-  second.style.height = '50%' ;
-  second.style.transform = 'translate(0,100%)';
-  homescreen.querySelector('.fade-overlay').style.display = 'none';
-  homescreen.querySelector('iframe').style.visibility = 'visible';
+function gen_split(appWindow) {
+    if (!homescreen.classList.contains('active')) {
+        console.log(cslpre + ' on est pas dans le homescreen');
+        let active = document.querySelector('#windows .active');
+        if (app1 == null) {
+            console.log(cslpre + ' on a pas encore une app');
+            show2(active, null);
+        } else {
+            console.log(cslpre + ' on a déjà une app');
+            show2(app1, active);
+        }
+    } else {
+        console.log(cslpre + ' on retourne sur le home');
+        show1(null);
+    }
+    console.log(cslpre + ' on a retaillé');
 }
 
-function show1(first){
-  app1 = first ;
-  if(first==null)
-   first = homescreen;
-  app2 = null ;
-  console.log(cslpre+' on va modif les css vers 1');
-  first.style.transform = 'translate(0,0)';
- 	first.style.height = '100%';
-  first.querySelector('.fade-overlay').style.display = 'none';
-  first.querySelector('iframe').style.visibility = 'visible';
+function show2(first, second) {
+    app1 = first;
+    if (first == null)
+        first = homescreen;
+    app2 = second;
+    if (second == null)
+        second = homescreen;
+    console.log(cslpre + ' on va modif les css vers 2');
+    first.style.height = '50%';
+    second.style.transform = 'translate(0,0)';
+    second.style.height = '50%';
+    second.style.transform = 'translate(0,100%)';
+    homescreen.querySelector('.fade-overlay').style.display = 'none';
+    homescreen.querySelector('iframe').style.visibility = 'visible';
 }
 
+function show1(first) {
+    app1 = first;
+    if (first == null)
+        first = homescreen;
+    app2 = null;
+    console.log(cslpre + ' on va modif les css vers 1');
+    first.style.transform = 'translate(0,0)';
+    first.style.height = '100%';
+    first.querySelector('.fade-overlay').style.display = 'none';
+    first.querySelector('iframe').style.visibility = 'visible';
+}
 
-/*function gen_split(appWin){
-    console.log(cslpre+' homescreen actif !!!!');
-    window.addEventListener('appresize',function(event){ // appwindowmanager-activated , appopened
-        ------------------------------------------
-   //   console.log(active);
-   //   active.style.height = '50%';
-    });
-    
-}*/
 
 // Main
 removeAddon(); // for testing - Ensures that previous deployed addons are removed
 window.addEventListener(listeners.appresize.event, listeners.appresize.callback);
 navigator.mozApps.mgmt.addEventListener('enabledstatechange', listeners.enabledstatechange.callback);
-    
-homescreen = document.querySelector('#homescreen');
-console.log("on a le homescreen"+ homescreen);
+
 /*if(!homescreen.classList.contains('active')){
     console.log(cslpre+' homescreen pas actif !!!!!');
     var active = document.querySelector('#windows .active');
@@ -135,12 +131,12 @@ function removeAddon() {
         }
     }
 }
-      
-    
-  if (document.querySelector('.fxos-banner')) {
+
+
+if (document.querySelector('.fxos-banner')) {
     // Already injected, abort.
     return;
-  } else {
+} else {
     var body = document.querySelector('body');
     var fxosBanner = document.createElement('div');
     fxosBanner.classList.add('fxos-banner');
@@ -155,6 +151,6 @@ function removeAddon() {
     bannerText.textContent = 'Wow, you have an extension installed!';
 
     closeBtn.onclick = function() {
-    	fxosBanner.parentNode.removeChild(fxosBanner);
+        fxosBanner.parentNode.removeChild(fxosBanner);
     }
-  }
+}
